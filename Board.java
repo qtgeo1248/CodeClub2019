a9274bb337daf26f576d67c01336f0064252a2b8
qtgeo1248 <gzhou00@stuy.edu>
2019-08-05 03:01:18 -0400

    kinda tostring

---
+++ Board.java
@@ -0,0 +1,32 @@
+public class Board {
+    private int[][] board;
+
+    public Board() {
+        board = new int[3][3];
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board.length; j++) {
+                board[i][j] = 0;
+            }
+        }
+    }
+
+    public String toString() {
+        String ans = "";
+        for (int i = 0; i < board.length; i++) {
+            String line = "";
+            for (int j = 0; j < board[i].length; j++) {
+                if (board[i][j] == 0) {
+                    line += "  |";
+                } else if (board[i][j] == 1) {
+                    line += "XX|";
+                } else {
+                    line += "OO|";
+                }
+            }
+            line = line.substring(0, line.length() - 1) + "\n";
+            line += "--|--|--\n";
+            ans += line;
+        }
+        return ans.substring(0, ans.lastIndexOf("| ") + 1);
+    }
+}
---
+++ Driver.java
@@ -0,0 +1,6 @@
+public class Driver {
+    public static void main(String[] args) {
+        Board test = new Board();
+        System.out.println(test);
+    }
+}
