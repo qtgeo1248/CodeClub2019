0eda7292fac0eef5c56d0acf97d7b6b608bc6857
qtgeo1248 <gzhou00@stuy.edu>
2019-08-05 23:38:29 -0400

    made printboard

--- TicTacToe/Driver.java
+++ TicTacToe/Driver.java
@@ -1,6 +1,24 @@
 public class Driver {
+    public static void printBoard(int[][] board) {
+        String ans = "";
+        for (int i = 0; i < board.length; i++) {
+            String line = "";
+            for (int j = 0; j < board[i].length; j++) {
+                if (board[i][j] == 0) {
+                    line += "  |";
+                } else if (board[i][j] == 1) {
+                    line += "XX|";
+                } else {
+                    line += "OO|";
+                }
+            }
+            line = line.substring(0, line.length() - 1) + "\n";
+            line += "--|--|--\n";
+            ans += line;
+        }
+        System.out.println(ans.substring(0, ans.lastIndexOf("| ") + 1));
+    }
     public static void main(String[] args) {
-        Board test = new Board();
-        System.out.println(test);
+        
     }
 }
