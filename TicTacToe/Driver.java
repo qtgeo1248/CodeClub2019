db4df9423bdd1fa514b60e6c2192b824f0527118
qtgeo1248 <gzhou00@stuy.edu>
2019-08-05 23:40:05 -0400

    made every element 0

--- TicTacToe/Driver.java
+++ TicTacToe/Driver.java
@@ -19,6 +19,11 @@ public class Driver {
         System.out.println(ans.substring(0, ans.lastIndexOf("| ") + 1));
     }
     public static void main(String[] args) {
-        
+        int[][] board = new int[3][3];
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                board[i][j] = 0;
+            }
+        }
     }
 }
