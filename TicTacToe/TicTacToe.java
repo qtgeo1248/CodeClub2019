a58152a57790591cc73a922f6111de88c9b9be82
qtgeo1248 <gzhou00@stuy.edu>
2019-08-07 15:46:05 -0400

    did input

--- TicTacToe/TicTacToe.java
+++ TicTacToe/TicTacToe.java
@@ -2,9 +2,9 @@ import java.util.*;
 
 public class TicTacToe {
     public static void printBoard(int[][] board) {
-        String ans = "";
+        String ans = "  A  B  C\n";
         for (int i = 0; i < board.length; i++) {
-            String line = "";
+            String line = (i + 1) + " ";
             for (int j = 0; j < board[i].length; j++) {
                 if (board[i][j] == 0) {
                     line += "  |";
@@ -15,15 +15,16 @@ public class TicTacToe {
                 }
             }
             line = line.substring(0, line.length() - 1) + "\n";
-            line += "--|--|--\n";
+            line += "  --|--|--\n";
             ans += line;
         }
         /* prints out
-        XX|XX|XX
-        --|--|--
-        XX|XX|XX
-        --|--|--
-        XX|XX|XX
+          A  B  C
+        1 XX|XX|XX
+          --|--|--
+        2 XX|XX|XX
+          --|--|--
+        3 XX|XX|XX
         */
         System.out.println(ans.substring(0, ans.lastIndexOf("| ") + 1));
     }
@@ -67,9 +68,37 @@ public class TicTacToe {
                 board[i][j] = 0;
             }
         }
+        printBoard(board);
         int player = 1;
+        Scanner input = new Scanner(System.in);
         while (winner(board) == 0) {
-
+            System.out.println("Welcome to Tic Tac Toe.\nPress any key to continue");
+            String key = input.nextLine();
+            System.out.println("Player 1 (X) goes first.\nPick a square:");
+            String square = input.nextLine();
+            int i = 0; //1, 2, or 3
+            int j = 0; //A, B, or C
+            boolean isValid = true;
+            if (square.contains("A")) {
+                j = 0;
+            } else if (square.contains("B")) {
+                j = 1;
+            } else if (square.contains("C")) {
+                j = 2;
+            } else {
+                isValid = false;
+                System.out.println("Invalid input. Please try again");
+            }
+            if (square.contains("1") && isValid) {
+                i = 0;
+            } else if (square.contains("2") && isValid) {
+                i = 1;
+            } else if (square.contains("3") && isValid) {
+                i = 2;
+            } else {
+                isValid = false;
+                System.out.println("Invalid input. Please try again");
+            }
         }
         System.out.println("Congradulations Player " + winner(board) + "!\nYou win!");
     }
